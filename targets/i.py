881ae9fb9340424c3a51e68bcce54b8aa6b69f0c
Madeleine Zurowski <madeleinezurowski@Madeleines-MacBook-Pro.local>
2024-01-31 10:07:54 -0500

    electron transition probs added as a target attribute. Also adjust model structure to remove requiring the defintion of dsigdER for rate

--- targets/i.py
+++ targets/i.py
@@ -1,90 +1,21 @@
 from target import Target
 import numpy as np
 
-##### All functions defined here should be unitless by design
+##### Load useful files (e.g., electron transition probabilities)
+i_10 = np.loadtxt("./targets/elec_prob/i_nl10.dat")
+i_20 = np.loadtxt("./targets/elec_prob/i_nl20.dat")
+i_21 = np.loadtxt("./targets/elec_prob/i_nl21.dat")
+i_30 = np.loadtxt("./targets/elec_prob/i_nl30.dat")
+i_31 = np.loadtxt("./targets/elec_prob/i_nl31.dat")
+i_32 = np.loadtxt("./targets/elec_prob/i_nl32.dat")
+i_40 = np.loadtxt("./targets/elec_prob/i_nl40.dat")
+i_41 = np.loadtxt("./targets/elec_prob/i_nl41.dat")
+i_42 = np.loadtxt("./targets/elec_prob/i_nl42.dat")
+i_50 = np.loadtxt("./targets/elec_prob/i_nl50.dat")
+i_51 = np.loadtxt("./targets/elec_prob/i_nl51.dat")
 
-class Na(Target):
-    def A(self):
-        return 23
+##### Define target functions
 
-    def FMpp(self,ER):
-        y = self.Y(ER)
-        return np.exp(-2.*y)*(120.-180.*y+87.*np.power(y,2.)-17.*np.power(y,3.)+1.2*np.power(y,4.))
-
-    def FMnp(self,ER):
-        y = self.Y(ER)
-        return np.exp(-2.*y)*(130.-200.*y+100.*np.power(y,2.)-20.*np.power(y,3.)+1.5*np.power(y,4.))
-
-    def FMpn(self,ER):
-        return self.FMnp(ER)
-
-    def FMnn(self,ER):
-        y = self.Y(ER)
-        return np.exp(-2.*y)*(140.-220.*y+120.*np.power(y,2.)-25.*np.power(y,3.)+1.8*np.power(y,4.))
-
-    def FS1pp(self,ER):
-        y = self.Y(ER)
-        return np.exp(-2.*y)*(0.273-0.824*y+1.19*np.power(y,2.)-0.477*np.power(y,3.)+0.0593*np.power(y,4.))
-
-    def FS1np(self,ER):
-        y = self.Y(ER)
-        return np.exp(-2.*y)*(0.0219-0.0578*y+0.036*np.power(y,2.)-0.003*np.power(y,3.)+0.000363*np.power(y,4.))
-
-    def FS1pn(self,ER):
-        return self.FS1np(ER)
-
-    def FS1nn(self,ER):
-        y = self.Y(ER)
-        return np.exp(-2.*y)*(0.00176-0.00396*y+0.00228*np.power(y,2.)-0.0000195*np.power(y,3.))
-
-    def FS2pp(self,ER):
-        y = self.Y(ER)
-        return np.exp(-2.*y)*(0.136-0.267*y+0.458*np.power(y,2.)-0.112*np.power(y,3.)+0.00828*np.power(y,4.))
-
-    def FS2np(self,ER):
-        y = self.Y(ER)
-        return np.exp(-2.*y)*(0.011-0.03*y+0.0217*np.power(y,2.)-0.00897*np.power(y,3.)+0.000592*np.power(y,4.))
-
-    def FS2pn(self,ER):
-        return self.FS2np(ER)
-
-    def FS2nn(self,ER):
-        y = self.Y(ER)
-        return np.exp(-2.*y)*(0.000882-0.0031*y+0.00399*np.power(y,2.)-0.00203*np.power(y,3.)+0.000409*np.power(y,4.))
-
-    def FDpp(self,ER):
-        y = self.Y(ER)
-        return np.exp(-2.*y)*(0.231-0.185*y+0.0502*np.power(y,2.))
-
-    def FDnp(self,ER):
-        y = self.Y(ER)
-        return np.exp(-2.*y)*(0.0812-0.065*y+0.0138*np.power(y,2.))
-
-    def FDpn(self,ER):
-        return self.FDnp(ER)
-
-    def FDnn(self,ER):
-        y = self.Y(ER)
-        return np.exp(-2.*y)*(0.0286-0.0228*y+0.00462*np.power(y,2.))
-
-    def FS1Dpp(self,ER):
-        y = self.Y(ER)
-        return np.exp(-2.*y)*(-0.25+0.48*y-0.29*np.power(y,2.)+0.049*np.power(y,3.))
-
-    def FS1Dnp(self,ER):
-        y = self.Y(ER)
-        return np.exp(-2.*y)*(-0.02+0.031*y-0.0076*np.power(y,2.)+0.00027*np.power(y,3.))
-
-    def FS1Dpn(self,ER):
-        y = self.Y(ER)
-        return np.exp(-2.*y)*(-0.088+0.17*y-0.081*np.power(y,2.)+0.011*np.power(y,3.))
-
-    def FS1Dnn(self,ER):
-        y = self.Y(ER)
-        return np.exp(-2.*y)*(-0.0071+0.011*y-0.003*np.power(y,2.))
-    
-
-#########################################################################################
 class I(Target):
     def A(self):
         return 127
@@ -164,3 +95,11 @@ class I(Target):
     def FS1Dnn(self,ER):
         y = self.Y(ER)
         return np.exp(-2.*y)*(-0.04+0.22*y-0.43*pow(y,2.)+0.38*pow(y,3.)-0.19*pow(y,4.)+0.053*pow(y,5.)-0.009*pow(y,6.)+0.00088*pow(y,7.))
+    
+    def eNL(self):
+        return [3.3E4,5.1E3,4.6E3,1.0E3,8.7E2,6.1E2,1.8E2,1.3E2,5.1E1,1.9E1,8.8]
+
+    def eTransEM(self):
+        return [i_10,i_20,i_21,i_30,i_31,i_32,i_40,i_41,i_42,i_50,i_51]
+
+    
