ced141933a623d3674e01dc6df9d699649e20671
ostanley <ostanley@student.unimelb.edu.au>
2024-02-12 18:58:08 +1100

    Added Spin Dependant model for MIGDAL.

--- models/migdal.py
+++ models/migdal.py
@@ -21,12 +21,12 @@ class MIGDAL(DMModel):
         
         vmins = [] # VMIN is returned as a list based on the transition energies in the target.
         for Eb in Target.eNL():
-            vmins.append(c*1e-3*(Target.mT()*ER+Target.mu_T(mX)*(-Eb+EM))/(Target.mu_T(mX)*np.power(2*Target.mT()*ER,0.5)))
+            vmins.append(c*1e-3*(Target.mT()*ER+Target.mu_T(mX)*(EM-Eb))/(Target.mu_T(mX)*np.power(2*Target.mT()*ER,0.5)))
         
         return vmins
         # return c*1e-3*(Target.mT()*ER+Target.mu_T(mX)*(EM))/(Target.mu_T(mX)*np.power(2*Target.mT()*ER,0.5))
  
-    def dR2dEREE(self, Target, mX, ER, sig, VelDist, EM):
+    def dR2dEREE(self, Target, mX, ER, sig, VelDist, EM, SI = True, cp=1/np.sqrt(2), cn = 1/np.sqrt(2), jx = 1/2, Helm = True):
         """
         For this model, we just take coupling of n and p to be equal, and the only operator we care about is O1
         [mX]  = [eV] DM mass
@@ -38,10 +38,16 @@ class MIGDAL(DMModel):
         Output units: [cm^2]/[eV] 
         """
 
-        FF = Target.Helm(ER)**2
-        dsigdER = (1/kg_to_eV)*sig*FF*Target.A()**2 / (2*Target.N_T()*Target.mu_T(mX)**2) 
-            # units of [cm^2]/[eV]
-        prefac = cpd_conversion*Target.N_T()*dsigdER*VelDist.rho/mX
+        if SI:
+            if Helm:
+                FF = Target.Helm(ER)**2
+            else:
+                FF = Target.F11(ER, cn=1/np.sqrt(2), cp=1/np.sqrt(2))
+        else:
+            FF = Target.F44(ER, cn,cp, jx)
+
+        dsigdER = (1/kg_to_eV)*sig*FF*Target.A()**2 / (2*Target.N_T()*Target.mu_T(mX)**2) # units of [cm^2]/[eV]
+        prefac = cpd_conversion*Target.N_T()*dsigdER*VelDist.rho/mX                       # 
 
         vmins = self.vmin(Target, mX, ER, EM)   # [km/s]       
         gdists = VelDist.gdist(vmins)           # Similarly taking and evaluating for the relevant vmin. [unitless]
@@ -52,12 +58,42 @@ class MIGDAL(DMModel):
         # return (prefac*np.array(gdists*probs), vmins, gdists, probs)
 
 
-    def dRdER(self, Target, E, mX, sig, VelDist, ERlim = 2*keV):
+    def dRdER(self, Target, E, mX, sig, VelDist, ERlim = 2*keV, **kwargs):
         return integrate.quad(lambda ER: self.dR2dEREE(Target, 
                                                         mX, 
                                                         ER, 
                                                         sig, 
                                                         VelDist,
-                                                        EM=E), 0, ERlim)[0]
+                                                        EM=E, 
+                                                        **kwargs), 0, ERlim)[0]
 
-""" Requre the addition of both a probability calculation and ionization rate calculation."""
+class SD_MIGDAL(DMModel): 
+    # This is a test branch/sanity check with this implementation we dont account for/need to include the Eem energy with that of the vmin. 
+    def vmin(self,Target,mX,ER):
+       """
+       [mX] = [eV] DM mass
+       [ER] = [eV] DM recoil energy
+
+       Output units: [km/s]
+       """
+       return (c*1.E-3)*np.abs((Target.mT()*ER/Target.mu_T(mX)))/np.power(2.*Target.mT()*ER,0.5)
+    
+    def dRdER(self,Target,ER,mX,sig,VelDist):
+        """
+        For this model, we just take coupling of n and p to be equal, and the only operator we care about is O1
+        [mX] = [eV] DM mass
+        [ER] = [eV] DM recoil energy
+        [sig] = [cm]^2 cross section
+
+        Output units: [cm^2]/[eV] 
+        """
+        # FF = Target.F11(ER,1/np.sqrt(2),1/np.sqrt(2)) ## form factor with couplings. Note that proton and neutron couplings are normalised to 1\
+        FF = Target.F44(ER, 1, 0, 0.5)
+        cross_sec = sig*2 ## as we normalise the nucleon vector [cn, cp] our built in cross section is sigma_N. Multiply by 2 to get this from sigma_p (which is generally assumed to be the input)
+        dsigdER = cross_sec*FF*Target.mT()/(2*Target.mu_N(mX)*Target.mu_N(mX)) ## units of [cm^2]/[eV]
+        vm = self.vmin(Target,mX,ER)
+        return cpd_conversion*Target.N_T()*(VelDist.rho/mX)*dsigdER*VelDist.gdist(vm)
+
+    def dRdE(self, Target, mX, sig, VelDist, EE):
+        return integrate.quad(lambda ER: self.dRdER(Target, ER,mX, sig, VelDist), a=0, b=np.inf)[0]*(1/np.pi)*np.array(Target.eTrans(EE))
+    
