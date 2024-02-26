18b6db9d536dcbfe4a0b659af0602136793d89d0
Samarth <72678609+samarth423@users.noreply.github.com>
2024-02-26 13:45:09 +1100

    Delete constants.py

--- constants.py
+++
@@ -1,8 +0,0 @@
-eV = 1 # base mass units
-keV = 1E3 
-GeV = 1E9
-
-c = 299792458. # speed of light [km/s]
-mp =  0.9314941*GeV # mass of a nulceon in eV.
-cpd_conversion = 86400*c*1E14 # for conversion of dsigdER to units of cpd/kg/keV
-kg_to_eV = 1.8E-36 # conversion between eV and kg
