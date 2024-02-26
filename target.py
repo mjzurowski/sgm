Add Migdal model, with fixes from Owen's branch (scaling, cross-section, integration range, energy transformations).

target.py
@@ -6,6 +6,12 @@ from constants import *
 ##### Note that a lot of these functions depend on additional data. These are included in subfolders in targets/, where info.txt files can be found that give detail on their units and source
 
 class Target(ABC):
+    def __init__(self, quenching_factor=None):
+        self.qf = quenching_factor
+
+    def quenching_factor(self):
+        return self.qf
+
     def spin_dep(self, jx):
         return (4*jx/3)*(jx+1) #### double check this defn as I think it should depend on the target
     
@@ -254,8 +260,8 @@ class Target(ABC):
         Should still be a list of the same length (and in units of [eV]^-1) as it later needs to be paired with appropriate vmins
         """
         trans_list = []
-        for i in range(0,len(self.eNL)):
-            trans_list.append(np.interp(E_EM-self.eNL[i],self.eTransE_E[i][:,0],self.eTransE_E[i][:,1])*np.heaviside(E_EM-self.eNL[i],1))
+        for i in range(0,len(self.eNL())):
+            trans_list.append(np.interp(E_EM - self.eNL()[i], self.eTransE_E()[i][:,0], self.eTransE_E()[i][:,1]) * np.heaviside(E_EM - self.eNL()[i], 1))
         return trans_list
     
 ##### Photoelectric absorption

veldists.py
@@ -9,20 +9,27 @@ class VelDist:
         Requires a dist name defined in the velocity distributions files
         Assume that we have both h and g distributions for a given distribution
         """
+        self.dist = dist
         self.rho = rho # DM density [GeV/cm3]
-        self.gfile= np.loadtxt("./velocity_distributions/g"+dist+".dat")
-        self.hfile= np.loadtxt("./velocity_distributions/h"+dist+".dat")
+        self.gfile= np.loadtxt(f'./velocity_distributions/g{self.dist}.dat')
+        self.hfile= np.loadtxt(f'./velocity_distributions/h{self.dist}.dat')
 
     def gdist(self,vm):
         """
         Interpolate given data to find the integral value for the min velocity vm
         g integral files are typically given in units of [s/km] but we require our values to be unitless so include appropriate c factors
         """
-        return np.interp(vm,self.gfile[:,0],self.gfile[:,1],right=0)*c*1E-3
+        return np.interp(vm, self.gfile[:, 0], self.gfile[:, 1], right=0) * c * 1E-3
     
     def hdist(self,vm):
         """
         Interpolate given data to find the integral value for the min velocity vm
         h integral files are typically given in units of [km/s] but we require our values to be unitless so include appropriate c factors
         """
-        return np.interp(vm,self.hfile[:,0],self.hfile[:,1],right=0)/(c*1E-3)
+        return np.interp(vm, self.hfile[:, 0], self.hfile[:, 1], right=0) / (c * 1E-3)
+
+    def vesc_km_s(self):
+        try:
+            return vesc_km_s[self.dist]
+        except Exception:
+            raise RuntimeError(f'Could not find escape velocity for {self.dist}')