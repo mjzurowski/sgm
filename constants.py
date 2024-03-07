dd0b4eca35b892b5d5af782933b9d217146ce703
guangyongfu <guangyongfu@guangyongfus-MacBook-Air.local>
2024-03-07 15:06:59 +1100

    changed smearing function

--- constants.py
+++ constants.py
@@ -7,12 +7,15 @@ c = 299792458. # speed of light [km/s]
 mp =  0.9314941*GeV # unified atomic mass
 cpd_conversion = 86400*c*1E14 # for conversion of dsigdER to units of cpd/kg/keV
 kg_to_eV = 1.8E-36 # conversion between eV and kg
+kpc_to_cm = 3.086e+21 #1 kpc [cm]
 
 Gf = 1.166364*1e-5 ##Fermi constant in natural unit GeV
 sin2thetaW = 0.2229 ## Weak mixing angle from https://physics.nist.gov/cgi-bin/cuu/Value?sin2th
-mproton=938.272*MeV
-mn=939.565*MeV
-mC12=12*mp
+mproton = 938.272*MeV
+mn = 939.565*MeV
+mC12 = 12*mp
 Delta_np = (mn-mproton)
-M_nucleus=(mproton+mn)/2
-me= 0.511*MeV
+M_nucleus = (mproton+mn)/2
+me = 0.511*MeV
+
+erg = 624.15*GeV
