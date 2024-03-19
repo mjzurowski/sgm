1ec187d706ad2566edaa847a3f227ec9608b519d
guangyongfu <guangyongfu@guangyongfus-MacBook-Air.local>
2024-03-19 17:24:30 +1100

    some cleanup

---
+++ detectors/lab.py
@@ -0,0 +1,40 @@
+import numpy as np
+from detector import Detector
+from targets.na import Na
+from targets.i import I
+from targets.h import H
+from targets.c import C
+from constants import *
+
+class LAB(Detector):
+    def Nuclei(self):
+        return [H(),C()]
+    
+    def ER_E(self,E):
+        ### This function will ultimately need some way to distinguish between NR and ER
+        ### For now can just avoid that by creating a separate background spec
+        """
+        [E] = [keV_0] Observed energy keV
+
+        Output units: [eV] recoil energy keV
+        """
+        return [E*keV/0.3, E*keV/0.09]
+    
+    def dERdE(self,E):
+        return [1/0.3, 1/0.09] ### ideally this should just be computed automatically in detector.py
+    
+    def ROI(self):
+        return [0,6]
+    
+    def Emax(self):
+        return 20
+    
+    def DeltaE(self,E):
+        return (0.488*pow(E,0.5))+(0.0091*E)
+    
+    def Res(self,E1,E2):
+        A = 1./(np.sqrt(2.*np.pi)*self.DeltaE(E2))
+        return A*np.exp(-0.5*pow((E1 - E2)/self.DeltaE(E2), 2.))
+    
+    def Eff(self,E):
+        return np.where(E>8,1,0.0429*E+0.657 ) 
--- snvmodels/neutrino.py
+++ snvmodels/neutrino.py
@@ -36,26 +36,26 @@ class Neutrino(NeutrinoModel):
 #            result = 
         elif source == "solar_f17":
             f17NDist = SolarNDist("f17",0.0005912e10)
-            result=f17NDist.dist(Ev/MeV)/MeV*24*3600
+            result=f17NDist.dist(Ev/MeV)/MeV
         elif source == "solar_o15":
             o15NDist = SolarNDist("o15",0.05031e10)
-            result=o15NDist.dist(Ev/MeV)/MeV*24*3600 
+            result=o15NDist.dist(Ev/MeV)/MeV
         elif source == "solar_n13":
             n13NDist = SolarNDist("n13",0.05712e10)
-            result=n13NDist.dist(Ev/MeV)/MeV*24*3600
+            result=n13NDist.dist(Ev/MeV)/MeV
         elif source == "solar_hep":
             hepNDist = SolarNDist("hep",0.000000788e10)
-            result=hepNDist.dist(Ev/MeV)/MeV*24*3600
+            result=hepNDist.dist(Ev/MeV)/MeV
         elif source == "solar_b8":
             b8NDist = SolarNDist("b8",0.0005822e10)
-            result=b8NDist.dist(Ev/MeV)/MeV*24*3600
+            result=b8NDist.dist(Ev/MeV)/MeV
         elif source == "solar_pp":
             ppNDist = SolarNDist("pp",5.938e10)
-            result=ppNDist.dist(Ev/MeV)/MeV*24*3600
+            result=ppNDist.dist(Ev/MeV)/MeV
         elif source == "solar_be7":
-            result = sympy.DiracDelta(x-384.3e3)* 0.103*0.4857e10 + sympy.DiracDelta(x-861.8e3)* 0.897*0.4857e10
+            result = sympy.DiracDelta(x-384.3e3)* 0.103*0.4857e10 + sympy.DiracDelta(x-861.8e3)* 0.897*0.4857e10/MeV
         elif source =='solar_pep':
-            result = sympy.DiracDelta(x-1442e3)*0.01401e10
+            result = sympy.DiracDelta(x-1442e3)*0.01401e10/MeV
         else:
             print("Please choose the right flavor of neutrinos. Options are supernova_e,supernova_ea,supernova_mu,supernova_mua,supernova_tau,supernova_taua")
             result=0
@@ -72,11 +72,14 @@ class Neutrino(NeutrinoModel):
         """
         # Intergration over neutrino spectrum
         #proton, C12 and electron number density in LAB
-        Np=7e28 #per ton from https://arxiv.org/pdf/1712.06985.pdf
-        NC=4.4e28
+        ratio_HC=1.639 #https://arxiv.org/pdf/1507.05613.pdf page163
+        Np=1e6*ratio_HC*1.007/(ratio_HC*1.007+12.011)/1.007*6.02e23
+        NC=1e6*12.011/(ratio_HC*1.007+12.011)/12.011*6.02e23
+        #Np=7e28 #per ton from https://arxiv.org/pdf/1712.06985.pdf
+        #NC=4.4e28
         Ne=Np+6*NC
         
-        if channel == "IBD-naive": ##https://arxiv.org/pdf/astro-ph/0302055.pdf
+        if channel == "IBD-naive": 
             ##inverse beta decay cross section (IBD) https://arxiv.org/pdf/astro-ph/0302055.pdf
             def Sig(Ev, Eout, channel,source):
                 pep=np.sqrt(Eout**2-me**2)
@@ -89,7 +92,7 @@ class Neutrino(NeutrinoModel):
             else:
                 integral = 0
 
-        if channel == "IBD-naive+": ##https://arxiv.org/pdf/astro-ph/0302055.pdf
+        if channel == "IBD-naive+": 
             ##inverse beta decay cross section (IBD) https://arxiv.org/pdf/astro-ph/0302055.pdf
             def Sig(Ev, Eout, channel,source):
                 pep=np.sqrt(Eout**2-me**2)
@@ -118,24 +121,24 @@ class Neutrino(NeutrinoModel):
             
         elif channel == "eES":
             def Sig(Ev, Eout, channel,source):
-                if source == 'supernova_e' or source == 'supernova_ea':
+                if source == 'supernova_e' or source == 'supernova_ea' or source.split('_')[0]=='solar':
                     epsilonN=-0.5-sin2thetaW
                     epsilonP=-sin2thetaW
                 else:
                     epsilonN=0.5-sin2thetaW
                     epsilonP=-sin2thetaW
-                if source == 'supernova_e' or source == 'supernova_mu' or source == 'supernova_tau':
+                if source == 'supernova_e' or source == 'supernova_mu' or source == 'supernova_tau' or source.split('_')[0]=='solar':
                     cross_sec = 2*me*Gf**2/np.pi*(epsilonN**2+epsilonP**2*(1-Eout/Ev)**2-epsilonN*epsilonP*me*Eout/Ev**2)*0.3894*1e-45
                 else:
                     cross_sec = 2*me*Gf**2/np.pi*(epsilonP**2+epsilonN**2*(1-Eout/Ev)**2-epsilonN*epsilonP*me*Eout/Ev**2)*0.3894*1e-45
                 return cross_sec
-            def integrand(Ev,Ed,e_ave,channel):
+            def integrand(Ev,Ed,e_ave,channel,source):
                 return Ne * Sig( Ev, Ed, channel,source) * self.Flux_neutrino(Ev,source,1,e_ave,3,1)
             Emin=Ed/2+np.sqrt(Ed*(Ed+2*me))/2
-            integral = integrate.quad(lambda Evl:integrand(Evl,Ed,E_ave,channel),Emin,max(Ev))[0]
+            integral = integrate.quad(lambda Evl:integrand(Evl,Ed,E_ave,channel,source),Emin,max(Ev))[0]
             
-        elif channel == 'C12_NC':
-            w=15.1*MeV
+        elif channel == 'C12_NC':##https://www.sciencedirect.com/science/article/pii/0370157379900103
+            w=15.11*MeV
             beta=1
             kai=1
             def Sig(Ev,channel,source):
