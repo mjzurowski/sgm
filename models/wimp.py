15d081fe2173d7af1591581a7ee70f354fd5c40d
Raghda <raghdayak@gmail.com>
2024-07-05 11:21:54 -0400

    changed the name dRdER_New to dRdER in WIMPall

--- models/wimp.py
+++ models/wimp.py
@@ -60,7 +60,7 @@ class WIMPall(DMModel):
        return kms*np.abs((Target.mT()*ER/Target.mu_T(mX)))/np.power(2.*Target.mT()*ER,0.5)
 
 
-    def dRdER(self,Target,ER,mX,VelDist,Coeff,Lambda,jx):
+    def dRdER_Old(self,Target,ER,mX,VelDist,Coeff,Lambda,jx):
         vm = self.vmin(Target,mX,ER)
         exposure = 612 #kg days
         NA=6.022e26
@@ -91,7 +91,7 @@ class WIMPall(DMModel):
         return exposure*kev2Day*(VelDist.rho*1e9/(cm2eV**3))*dsigdER*((FF11+FF44+FF66+FF77[0]+FF88[0]+FF99+FF1010+FF1111+FF1212[0]+FF46+FF64+FF98+FF89+FF1112+FF1211)*VelDist.gdist(vm) + (FF77[1]+FF88[1]+FF1212[1])*VelDist.hdist(vm))
 
     
-    def dRdER_New(self,Target,ER,mX,VelDist,Coeff,Lambda,jx):
+    def dRdER(self,Target,ER,mX,VelDist,Coeff,Lambda,jx):
         vm = self.vmin(Target,mX,ER)
         exposure = 612 #kg days
         NA=6.022e26
@@ -131,26 +131,6 @@ class WIMPall(DMModel):
             FF1212 = Target.F1212(ER,c12(p,Coeff,mX,Lambda,mp),c12(n,Coeff,mX,Lambda,mp),vm/kms,jx) #g,h
             FF1112 = Target.F1112(ER,c11(p,Coeff,mX,Lambda,mp),c11(n,Coeff,mX,Lambda,mp),c12(p,Coeff,mX,Lambda,mp),c12(n,Coeff,mX,Lambda,mp),jx)
             FF1211 = Target.F1211(ER,c12(p,Coeff,mX,Lambda,mp),c12(n,Coeff,mX,Lambda,mp),c11(p,Coeff,mX,Lambda,mp),c11(n,Coeff,mX,Lambda,mp),jx)
-        
-        #FF11 = Target.F11(ER,c1(p,Coeff,mX,Lambda,mp),c1(n,Coeff,mX,Lambda,mp))
-        ##FF33 = Target.F33(ER,c3(p,Coeff,mX,Lambda),c3(n,Coeff,mX,Lambda),vm/kms)
-        #FF44 = Target.F44(ER,c4(p,Coeff,mX,Lambda,mp),c4(n,Coeff,mX,Lambda,mp),jx)
-        ##FF55 =
-        #FF66 = Target.F66(ER,c6(p,Coeff,mX,Lambda,mp),c6(n,Coeff,mX,Lambda,mp),jx)
-        #FF77 = Target.F77(ER,c7(p,Coeff,mX,Lambda,mp),c7(n,Coeff,mX,Lambda,mp),vm/kms) #g,h
-        #FF88 = Target.F88(ER,c8(p,Coeff,mX,Lambda,mp),c8(n,Coeff,mX,Lambda,mp),jx,vm/kms) #g,h
-        #FF99 = Target.F99(ER,c9(p,Coeff,mX,Lambda,mp),c9(n,Coeff,mX,Lambda,mp),jx)
-        #FF1010 = Target.F1010(ER,c10(p,Coeff,mX,Lambda,mp),c10(n,Coeff,mX,Lambda,mp))
-        #FF1111 = Target.F1111(ER,c11(p,Coeff,mX,Lambda,mp),c11(n,Coeff,mX,Lambda,mp),jx)
-        #FF1212 = Target.F1212(ER,c12(p,Coeff,mX,Lambda,mp),c12(n,Coeff,mX,Lambda,mp),vm/kms,jx) #g,h
-        ##FF13
-        ##FF45
-        #FF46 = Target.F46(ER,c4(p,Coeff,mX,Lambda,mp),c4(n,Coeff,mX,Lambda,mp),c6(p,Coeff,mX,Lambda,mp),c6(n,Coeff,mX,Lambda,mp),jx)
-        #FF64 = Target.F64(ER,c6(p,Coeff,mX,Lambda,mp),c6(n,Coeff,mX,Lambda,mp),c4(p,Coeff,mX,Lambda,mp),c4(n,Coeff,mX,Lambda,mp),jx)
-        #FF98 = Target.F98(ER,c9(p,Coeff,mX,Lambda,mp),c9(n,Coeff,mX,Lambda,mp),c8(p,Coeff,mX,Lambda,mp),c8(n,Coeff,mX,Lambda,mp),jx)
-        #FF89 = Target.F89(ER,c8(p,Coeff,mX,Lambda,mp),c8(n,Coeff,mX,Lambda,mp),c9(p,Coeff,mX,Lambda,mp),c9(n,Coeff,mX,Lambda,mp),jx)
-        #FF1112 = Target.F1112(ER,c11(p,Coeff,mX,Lambda,mp),c11(n,Coeff,mX,Lambda,mp),c12(p,Coeff,mX,Lambda,mp),c12(n,Coeff,mX,Lambda,mp),jx)
-        #FF1211 = Target.F1211(ER,c12(p,Coeff,mX,Lambda,mp),c12(n,Coeff,mX,Lambda,mp),c11(p,Coeff,mX,Lambda,mp),c11(n,Coeff,mX,Lambda,mp),jx)
         
         dsigdER = Target.mT() *NA/(32*math.pi*mX**3 *mp**2 *ABar)
         return exposure*kev2Day*(VelDist.rho*1e9/(cm2eV**3))*dsigdER*((FF11+FF44+FF66+FF77[0]+FF88[0]+FF99+FF1010+FF1111+FF1212[0]+FF46+FF64+FF98+FF89+FF1112+FF1211)*VelDist.gdist(vm) + (FF77[1]+FF88[1]+FF1212[1])*VelDist.hdist(vm))
